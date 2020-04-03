d10b0a1450468a8f1e827f6629280ef8a09c41dc
Sasidharan, Sandeep (ITCDCC) - KLM <Sandeep.Sasidharan@klm.com>
2020-04-03 06:46:07 +0200

    client files added

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
 function Job({ job }) {
+
   return (
     <div className="job">
-     { job.title }
-     { job.company }
+        <div>
+          <Typography variant="h6">{ job.title }</Typography>
+          <Typography variant="h5">{ job.company }</Typography>
+          <Typography>{ job.location }</Typography>
+      </div>
+      <div>
+  <Typography>{job.created_at.split(' ').slice(0, 3)}</Typography>
+      </div>
     </div>
   );
 }
--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,19 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
+
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
 import Job from './Job';
 
 function Jobs({jobs}) {
+
+  const numJobs = jobs.length;
+  const numPages = Math.ceil(numJobs / 50);
+
+  const [activeStep, setActiveStep] = useState(0);
+  const jobsOnPage = jobs.slice(activeStep * 50, (activeStep * 50) + 50);
+
+  function handleNext() {
+    setActiveStep (prevActiveStep => prevActiveStep + 1);
+  }
+
+  function handleBack() {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  }
+
+  console.log('job is ', jobs[0]);
   return (
     <div className="jobs">
-      <Typography variabnt="h1">
+      <Typography variant="h4" component="h1">
         Entry Level Software Jobs
       </Typography>
+      <Typography variant="h6" component="h1">
+        Found {numJobs} Jobs
+      </Typography>
      {
-       jobs.map(
+       jobsOnPage.map(
          job => <Job job={job} />
        )
      }
+
+  <div>Page {activeStep + 1} of {numPages }</div>
+
+     <MobileStepper
+      variant="dots"
+      steps={numPages}
+      position="static"
+      activeStep={activeStep}
+      nextButton={
+        <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+          Next
+          <KeyboardArrowLeft />
+          }
+        </Button>
+      }
+      backButton={
+        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+          {<KeyboardArrowRight />}
+          Back
+        </Button>
+      }
+    />
+
+    
     </div>
   );
 }
